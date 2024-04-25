4632c4c017610466bd17a3cfe4e96dede28b2ea7
Bruno Fernandes Campos <97699257+BrunoFerCam@users.noreply.github.com>
2024-04-25 11:00:26 -0300

    Envio do sistema de cadastro e leitura dos serviços e produtos
    Envio do sistema de cadastro e leitura dos serviços e dos produtos, além de acertar o modelo dos dois

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,8 +2,12 @@ import Entrada from "../io/entrada";
 import Empresa from "../modelo/empresa";
 import CadastroCliente from "../negocio/cadastro/cadastroCliente";
 import CadastroPet from "../negocio/cadastro/cadastroPet";
+import CadastroProduto from "../negocio/cadastro/cadastroProduto";
+import CadastroServico from "../negocio/cadastro/cadastroServico";
 import ListagemClientes from "../negocio/listar/listagemClientes";
 import ListagemPets from "../negocio/listar/listagemPets";
+import ListagemProdutos from "../negocio/listar/listagemProdutos";
+import ListagemServicos from "../negocio/listar/listagemServicos";
 
 console.log(`Bem-vindo ao melhor sistema de gerenciamento de pet shops e clínicas veterinarias`)
 let empresa = new Empresa()
@@ -17,6 +21,12 @@ while (execucao) {
     console.log(`--Pets--`);
     console.log(`3 - Cadastrar pet`);
     console.log(`4 - Listar todos os pets`);
+    console.log(`--Serviço--`);
+    console.log(`5 - Cadastrar serviço`);
+    console.log(`6 - Listar todos os serviço`);
+    console.log(`--Produto--`);
+    console.log(`7 - Cadastrar produto`);
+    console.log(`8 - Listar todos os produto`);
     console.log(`0 - Sair`);
 
     let entrada = new Entrada()
@@ -38,6 +48,22 @@ while (execucao) {
         case 4:
             let listagemPet = new ListagemPets(empresa.getPets)
             listagemPet.listar()
+            break;
+        case 5:
+            let listagemProduto = new ListagemProdutos(empresa.getProdutos)
+            listagemProduto.listar()
+            break;
+        case 6:
+            let cadastroProduto = new CadastroProduto(empresa.getProdutos)
+            cadastroProduto.cadastrar()
+            break;
+        case 7:
+            let listagemServico = new ListagemServicos(empresa.getServicos)
+            listagemServico.listar()
+            break;
+        case 8:
+            let cadastroServico = new CadastroServico(empresa.getServicos)
+            cadastroServico.cadastrar()
             break;
         case 0:
             execucao = false
--- src/modelo/servico.ts
+++ src/modelo/servico.ts
@@ -1,3 +1,15 @@
 export default class Servico {
-    public nome!: string
+    public nome: string
+    public tipo: string
+    public valor: string
+
+    constructor(nome: string, tipo: string, valor: string) {
+        this.nome = nome
+        this.tipo = tipo
+        this.valor = valor
+    }
+    public get getNome(): string {return this.nome}
+    public get getTipo(): string {return this.tipo}
+    public get getValor(): string {return this.valor}
+
 }
